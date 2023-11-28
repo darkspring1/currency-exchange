d5c2673aedb05610cc13ff57cb336ef6d62c197d
georgii.olt <olt.egor@gmail.com>
2023-11-29 02:45:12 +0300

    e2e tests for users and currencies

---
+++ CurrencyExchange/E2E.Tests/ControllerTests/CurrenciesTest.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Net;
+
+namespace E2E.Tests.ControllerTests;
+
+public class CurrenciesTest : IClassFixture<WebApplicationFactory<Program>>
+{
+
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public CurrenciesTest(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task GetCurrencies_Success()
+    {
+        var client = _factory.CreateClient();
+        var response = await client.GetAsync("/currencies");
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
