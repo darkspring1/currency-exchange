78ee6c899533b61df789c42295615be755f807da
georgii.olt <olt.egor@gmail.com>
2023-11-29 23:07:13 +0300

    fix exchange request

--- CurrencyExchange/Api/Models/ExchangeRequest.cs
+++ CurrencyExchange/Api/Models/ExchangeRequest.cs
@@ -2,6 +2,8 @@
 
 public class ExchangeRequest
 {
+    public Guid IdempotencyKey { get; set; }
+    public Guid UserId { get; set; }
     public string From { get; set; }
     public string To { get; set; }
     public decimal Amount { get; set; }
