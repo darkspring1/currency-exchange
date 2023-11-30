1ed2a6e907c05df09af7e9aef18244bf707880ac
georgii.olt <olt.egor@gmail.com>
2023-11-30 19:18:26 +0300

    balance controller

---
+++ CurrencyExchange/Api/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using BussinesServices.ServiceResult;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    public class BaseController: ControllerBase
+    {
+        protected ActionResult ServiceResult<T>(IResult<T> result)
+        {
+            if (result.Error != null)
+            {
+                return BadRequest(result.Error);
+            }
+
+            if(result.Data == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result.Data);
+        }
+    }
+}
