bf85edb05080e819ca069349bf912f566536c378
georgii.olt <olt.egor@gmail.com>
2023-12-01 01:52:29 +0300

    test fix

--- CurrencyExchange/E2E.Tests/ControllerTests/ExchangeTest.cs
+++ CurrencyExchange/E2E.Tests/ControllerTests/ExchangeTest.cs
@@ -10,27 +10,47 @@ public class ExchangeTest(WebApplicationFactory<Program> factory) : E2EBaseTest(
     [Fact]
     public async Task Exchange_Success()
     {
-        const decimal fromBalance = 100;
+        const decimal fromBalance = 1000;
         const decimal toBalance = 3;
-        const decimal usdToRubRate = 0.011m;
+        const decimal rate = 0.011m; //rub -> usd
+        const decimal fee = 0.05m;
+        const decimal exchangeAmount = 100;
+        
+        const decimal expectedFromBalance = 1000 - exchangeAmount;
+        const decimal toAmount = exchangeAmount * rate;
+        const decimal feeAmount = toAmount*fee;
+
+        const decimal expectedToBalance = toBalance + toAmount - feeAmount;
+        
+        
         var newUser = await Client.CreateRandomUserAsync();
         await Task.WhenAll(Client.CreateUsdAsync(), Client.CreateRubAsync());
         
         await Task.WhenAll(
-            Client.SetUserBalanceAsync(newUser.Id.ToString(), "usd", fromBalance),
-            Client.SetUserBalanceAsync(newUser.Id.ToString(), "rub", toBalance));
+            Client.SetUserBalanceAsync(newUser.Id.ToString(), "rub", fromBalance),
+            Client.SetUserBalanceAsync(newUser.Id.ToString(), "usd", toBalance));
 
+        var idempotencyKey = Guid.NewGuid();
         var exchangeResponse = await Client.ExchangeAsync<ExchangeResponseDto>(new ExchangeRequestDto
         {
-            IdempotencyKey = Guid.NewGuid(),
-            Amount = 100,
+            IdempotencyKey = idempotencyKey,
+            Amount = exchangeAmount,
             UserId = newUser.Id,
-            Fee = 5,
-            Rate = usdToRubRate,
-            From = "usd",
-            To = "rub"
+            Fee = fee,
+            Rate = rate,
+            From = "rub",
+            To = "usd"
         });
 
         Assert.NotNull(exchangeResponse);
+        Assert.Equal(idempotencyKey, exchangeResponse.IdempotencyKey);
+        Assert.Equal(exchangeAmount, exchangeResponse.FromAmount);
+        Assert.Equal(toAmount, exchangeResponse.ToAmount);
+        Assert.Equal(newUser.Id, exchangeResponse.UserId);
+        Assert.Equal("RUB", exchangeResponse.From);
+        Assert.Equal("USD", exchangeResponse.To);
+        Assert.Equal(fee, exchangeResponse.Fee);
+        Assert.Equal(feeAmount, exchangeResponse.FeeAmount);
+        Assert.Equal(rate, exchangeResponse.Rate);
     }
 }
