0d441a8683d5c7af4240e026b70edb187ca43d64
georgii.olt <olt.egor@gmail.com>
2023-12-01 02:14:29 +0300

    refactoring

--- CurrencyExchange/E2E.Tests/ControllerTests/ExchangeTest.cs
+++ CurrencyExchange/E2E.Tests/ControllerTests/ExchangeTest.cs
@@ -1,5 +1,4 @@
-﻿using Api.Controllers;
-using BussinesServices.Dto;
+﻿using BussinesServices.Dto;
 using E2E.Tests.Extensions;
 using Microsoft.AspNetCore.Mvc.Testing;
 
@@ -8,50 +7,31 @@ namespace E2E.Tests.ControllerTests;
 public class ExchangeTest(WebApplicationFactory<Program> factory) : E2EBaseTest(factory)
 {
     [Fact]
-    public async Task Exchange_Success()
+    public async Task Success()
     {
-        const decimal fromBalance = 1000;
-        const decimal toBalance = 3;
-        const decimal rate = 0.011m; //rub -> usd
-        const decimal fee = 0.05m;
-        const decimal exchangeAmount = 100;
-        
-        const decimal expectedFromBalance = 1000 - exchangeAmount;
-        const decimal toAmount = exchangeAmount * rate;
-        const decimal feeAmount = toAmount*fee;
-
-        const decimal expectedToBalance = toBalance + toAmount - feeAmount;
-        
-        
-        var newUser = await Client.CreateRandomUserAsync();
-        await Task.WhenAll(Client.CreateUsdAsync(), Client.CreateRubAsync());
-        
-        await Task.WhenAll(
-            Client.SetUserBalanceAsync(newUser.Id.ToString(), "rub", fromBalance),
-            Client.SetUserBalanceAsync(newUser.Id.ToString(), "usd", toBalance));
-
+        var newUser = await CreateUserAsync();
         var idempotencyKey = Guid.NewGuid();
         var exchangeResponse = await Client.ExchangeAsync<ExchangeResponseDto>(new ExchangeRequestDto
         {
             IdempotencyKey = idempotencyKey,
-            Amount = exchangeAmount,
+            Amount = ExchangeAmount,
             UserId = newUser.Id,
-            Fee = fee,
-            Rate = rate,
+            Fee = Fee,
+            Rate = Rate,
             From = "rub",
             To = "usd"
         });
 
         Assert.NotNull(exchangeResponse);
         Assert.Equal(idempotencyKey, exchangeResponse.IdempotencyKey);
-        Assert.Equal(exchangeAmount, exchangeResponse.FromAmount);
+        Assert.Equal(ExchangeAmount, exchangeResponse.FromAmount);
         Assert.Equal(toAmount, exchangeResponse.ToAmount);
         Assert.Equal(newUser.Id, exchangeResponse.UserId);
         Assert.Equal("RUB", exchangeResponse.From);
         Assert.Equal("USD", exchangeResponse.To);
-        Assert.Equal(fee, exchangeResponse.Fee);
+        Assert.Equal(Fee, exchangeResponse.Fee);
         Assert.Equal(feeAmount, exchangeResponse.FeeAmount);
-        Assert.Equal(rate, exchangeResponse.Rate);
+        Assert.Equal(Rate, exchangeResponse.Rate);
         
         var balances = await Task.WhenAll(
             Client.GetUserBalanceAsync<BalanceResponseDto>(newUser.Id.ToString(), "rub"),
@@ -68,4 +48,28 @@ public class ExchangeTest(WebApplicationFactory<Program> factory) : E2EBaseTest(
         AssertBalance(expectedFromBalance, "RUB", balances[0]);
         AssertBalance(expectedToBalance, "USD", balances[1]);
     }
+
+    private async Task<UserResponseDto> CreateUserAsync()
+    {
+        var newUser = await Client.CreateRandomUserAsync();
+        await Task.WhenAll(Client.CreateUsdAsync(), Client.CreateRubAsync());
+        
+        await Task.WhenAll(
+            Client.SetUserBalanceAsync(newUser.Id.ToString(), "rub", FromBalance),
+            Client.SetUserBalanceAsync(newUser.Id.ToString(), "usd", ToBalance));
+
+        return newUser;
+    }
+
+    const decimal FromBalance = 1000;
+    const decimal ToBalance = 3;
+    const decimal Rate = 0.011m; //rub -> usd
+    const decimal Fee = 0.05m;
+    const decimal ExchangeAmount = 100;
+        
+    const decimal expectedFromBalance = 1000 - ExchangeAmount;
+    const decimal toAmount = ExchangeAmount * Rate;
+    const decimal feeAmount = toAmount * Fee;
+
+    const decimal expectedToBalance = ToBalance + toAmount - feeAmount;
 }
